356deff2dbaa41794fab003370a3374e33318479
zhanghui <1577107936@qq.com>
2017-03-31 00:09:46 +0800

    提交package样例

---
+++ codelearning/src/com/rookiego/before/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.rookiego.before;
+
+/**
+ * @author zhh
+ * @date 2017年3月31日
+ * @time 上午12:06:52
+ * @version JDK 1.7
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java world, May you have a good trip!");
+	}
+
+}
