13e8e52b757d792fc56853e99bd2d38e365cc189
LiuYabo <489493660@qq.com>
2017-04-04 12:52:36 +0800

    第一天作业

---
+++ codelearning/src/com/rookiego/quiz/day1/outputhelloworld.java
@@ -0,0 +1,10 @@
+package com.rookiego.quiz.day1;
+
+public class outputhelloworld {
+	public static void main(String[] args) {
+		String world = "Hello World!";
+		System.out.println(world);
+
+	}
+
+}
