b003b25d295c196dff3e66e42d122c63e7b738dc
LiuYabo <489493660@qq.com>
2017-04-06 10:13:33 +0800

    第三天修改

--- codelearning/src/com/rookiego/quiz/day2/TestOutPutILoveJava.java
+++ codelearning/src/com/rookiego/quiz/day2/TestOutPutILoveJava.java
@@ -2,7 +2,6 @@ package com.rookiego.quiz.day2;
 
 public class TestOutPutILoveJava {
 	public static void main(String[] args) {
-		String ilovejava = "I Love Java !";
-		System.out.println(ilovejava);
+		System.out.println(args[0]);
 	}
 }
