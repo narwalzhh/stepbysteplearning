1b7c8be1c3eb02bd11380757773ce78e4f072745
LiuYabo <489493660@qq.com>
2017-04-10 09:58:18 +0800

    day3代码提交

--- codelearning/src/com/rookiego/quiz/day3/TestYangTriangle.java
+++ codelearning/src/com/rookiego/quiz/day3/TestYangTriangle.java
@@ -8,19 +8,16 @@ public class TestYangTriangle {
 		for (int i = 0; i < 10; i++) {
 			number[i] = new int[i + 1];
 			number[i][0] = 1;
-			number[i][i] = 1;
-		}
-		for (int i = 2; i < 10; i++) {
-			for (int j = 1; j != i; j++) {
+			for (int j = 1; j < i; j++) {
 				number[i][j] = number[i - 1][j - 1] + number[i - 1][j];
 			}
 		}
+
 		for (int i = 0; i < 10; i++) {
-			for (int a = 0; a<9-i;a++) {
-				System.out.print(' ');
+			for (int a = 0; a < 9 - i; a++) {
+				System.out.print(" ");
 			}
 			for (int j = 0; j != i; j++) {
-				
 
 				System.out.print(number[i][j] + " ");
 			}
@@ -29,4 +26,4 @@ public class TestYangTriangle {
 		}
 	}
 }
-//如何输出居中还没想到，输出方式不尽人意,输出也有误，回来就改
+// 如何输出居中还没想到，输出方式不尽人意,循环嵌套太多
